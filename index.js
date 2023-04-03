4536cd4aefb83062ec9c55ae72059a0ca46a8d05
charity <charity.joseph@students.moringaschool.com>
2023-04-03 11:24:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser="Charity Joseph";
+const welcomeMessage='Welcome to Flatbook,${currentUser}';
+const excitedWelcome= welcomeMessage.toUpperCase();
+const shortGreeting =`Welcome,${currentUser.slice(0,1)}`;
